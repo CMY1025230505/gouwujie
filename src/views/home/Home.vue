d7e6b9e7f13b34e17b510be2bb83cf99deeaee62
changmingyuan <1025230505@qq.com>
2021-03-01 18:21:35 +0800

    2021/3/1

---
+++ src/views/home/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+  import NavBar from 'components/common/navBar/NavBar'
+
+  export default {
+    name:'Home',
+    components: {
+      NavBar,
+      HomeSwiper
+    },
+    created() {
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list
+      })
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: [],
+      }
+    },
+  }
+
+</script>
+<style scoped>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: white;
+  }
+</style>
