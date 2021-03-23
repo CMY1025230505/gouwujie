4e90990440866689ca4793a59e502d622bed7437
changmingyuan <1025230505@qq.com>
2021-03-23 10:26:38 +0800

    3.23

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,38 +1,96 @@
 <template>
   <div id="detail">
-    <detail-nav-bar></detail-nav-bar>
-    <detail-swiper :topImage="topImage"></detail-swiper>
+    <detail-nav-bar class="detail-nav"></detail-nav-bar>
+    <scroll class="content" ref="scroll">
+      <detail-swiper :topImage="topImage"></detail-swiper>
+      <detail-base-info :goods="goods"></detail-base-info>
+      <detail-shop-info :shop="shop"></detail-shop-info>
+      <detail-goods-info
+        :detailInfo="detailInfo"
+        @imgLoad="imgLoad"
+      ></detail-goods-info>
+      <detail-param-info :param-info="paramInfo"></detail-param-info>
+    </scroll>
   </div>
 </template>
 
 <script>
-  import DetailNavBar from './childComps/DetailNavBar'
-  import DetailSwiper from './childComps/DetailSwiper'
-  import { getDetail } from "network/detail";
+import DetailNavBar from "./childComps/DetailNavBar";
+import DetailSwiper from "./childComps/DetailSwiper";
+import DetailBaseInfo from "./childComps/DetailBaseInfo";
+import DetailShopInfo from "./childComps/DetailShopInfo";
+import DetailGoodsInfo from "./childComps/DetailGoodsInfo";
+import DetailParamInfo from "./childComps/DetailParamInfo";
 
-  export default {
-    name:'Detail',
-    props:[''],
-    data () {
-      return {
-        iid: null,
-        topImage: null
-      };
-    },
-    created() {
-      this.iid = this.$route.params.iid
+import Scroll from "components/common/scroll/Scroll";
 
-      getDetail(this.iid).then(res => {
-        this.topImage = res.result.itemInfo.topImages
-      })
-    },
-    components: {
-      DetailNavBar,
-      DetailSwiper
+import { getDetail, Goods, Shop, GoodsParam } from "network/detail";
+
+export default {
+  name: "Detail",
+  props: [""],
+  data() {
+    return {
+      iid: null,
+      topImage: null,
+      goods: {},
+      shop: {},
+      detailInfo: {},
+      paramInfo: {}
+    };
+  },
+  methods: {
+    imgLoad() {
+      this.$refs.scroll.refresh()
     }
-  }
+  },
+  created() {
+    this.iid = this.$route.params.iid;
 
+    getDetail(this.iid).then((res) => {
+      const data = res.result;
+      console.log(data);
+      this.topImage = data.itemInfo.topImages;
+
+      this.goods = new Goods(
+        data.itemInfo,
+        data.columns,
+        data.shopInfo.services
+      );
+      this.shop = new Shop(data.shopInfo);
+      this.detailInfo = data.detailInfo;
+      this.paramInfo = new GoodsParam(data.itemParams.info, data.itemParams.rule)
+    });
+  },
+  components: {
+    DetailNavBar,
+    DetailSwiper,
+    DetailBaseInfo,
+    DetailShopInfo,
+    Scroll,
+    DetailGoodsInfo,
+    DetailParamInfo
+  },
+};
 </script>
-<style lang='' scoped>
-
+<style scoped>
+#detail {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+  height: 100vh;
+}
+.content {
+  position: absolute;
+  top: 44px;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  overflow: hidden;
+}
+.detail-nav {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+}
 </style>
